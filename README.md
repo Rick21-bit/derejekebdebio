a1e0821111beb461648547aae4b669000c5b9738
Rick21-bit <Rick21-bit@users.noreply.github.com>
2023-09-14 20:00:00 +0300

    vue landing page for dereje kebede bio and lyrics links

---
+++ README.md
@@ -0,0 +1,16 @@
+# Dereje Kebede Bio
+
+A Vue landing page for Dr. Dereje Kebede, an Ethiopian gospel singer whose music became my favorite in this season.
+
+## Live site
+
+Hosted on GitHub Pages.
+
+## Data
+
+Biography and discography are based on public sources, with links to the WikiMezmur lyrics database for full song lyrics.
+
+## Notes
+
+- Built with Vue 3 via CDN so it runs from a static host.
+- Lyrics are not embedded to respect rights; only song titles and external links are listed.
---
+++ app.js
@@ -0,0 +1,48 @@
+const { createApp, ref } = Vue;
+
+createApp({
+  setup() {
+    const bio = ref([
+      'Dr. Dereje Kebede was born in Addis Ababa around 1957/58. He grew up carrying heavy family responsibilities: his parents passed away when he was young, and his elder brother, songwriter Million Kebede, was killed during the Ethiopian Revolution.',
+      'A song by the Mulu Wongel Tsion Choir called "Heal me Jesus" changed his life and gave him the desire to serve God through music. He joined the choir of Meserete Kristos Church in Bole and later became a solo singer.',
+      'Dereje was the first Ethiopian gospel singer to publish an audio cassette. He also introduced electronic keyboard and Ethiopian traditional instruments into gospel recordings, shaping the sound of the genre.',
+      'During the military regime, he refused to compose revolutionary songs and stood against the state atheist ideology. Because of this, he was imprisoned for six months. In the early 1980s he left Ethiopia, first to Kenya and later to the United States.',
+      'He holds degrees in Agriculture, Religious Education, Addiction Science and Health Science. He worked as a nurse and now serves as a health policy consultant and addiction therapist.Through all of this, his songs have touched many Ethiopian Christians and beyond.'
+    ]);
+
+    const albums = ref([
+      {
+        title: 'Album 1',
+        amharic: 'አንደኛ ካሴት',
+        lyricsUrl: 'https://wikimezmur.org/am/Dereje_Kebede/Album_1',
+        songs: ['ምሥጋና ዕልልታ', 'ያለፈው በደሌን ተወው', 'ትዝ ይለኛል', 'መናህ ይጥመኛል', 'ቆሜ ብጠብቅህ እስከ መገለጥህ', 'የአንተን ድምጽ ስሰማ ነቃለሁ', 'ጌታን አመልካለሁ']
+      },
+      {
+        title: 'Mengeste Semay',
+        amharic: 'መንግሥተ ሰማይ',
+        lyricsUrl: 'https://wikimezmur.org/am/Dereje_Kebede',
+        songs: ['ድንቅ ሥራ', 'እግዚአብሔር ሰውን ቢሆን', 'መንገዴን በፊቱ አጸናለሁ', 'ብልህ ሰው ነፍሱን ያድናል', 'የንጋቱ ኮከብ አበራ', 'ውበት ይረግፋል', 'ማረን አቤቱ', 'መዳን ይሆንልናል', 'ሰላም አለኝ', 'ትህትና ሊያስተምረኝ', 'እኔስ በሕይወቴ ልበርታ']
+      },
+      {
+        title: 'Collection',
+        amharic: 'ስብስብ',
+        lyricsUrl: 'https://wikimezmur.org/am/Dereje_Kebede/Collection',
+        songs: ['አቤት እንዴት ታላቅ ነህ', 'አመልካለሁ', 'አምላኬ እረኛዬ ነው', 'አንድ ነገር', 'አዎን ያያል', 'ወንዙ ከፊት ሲታይህ', 'ድል በድል ጐዳና', 'ጌታ ቅደምልኝ', 'ጌታ ረዳቴ ነው', 'ግሩም ነው ድንቅ ነው', 'እንዴት ድንቅ አምላክ ነው', 'እኔን ጌታ ሆይ']
+      },
+      {
+        title: 'Adagnie Yesus',
+        amharic: 'አዳኜ ኢየሱስ',
+        lyricsUrl: 'https://wikimezmur.org/am/Dereje_Kebede',
+        songs: ['አዳኜ ኢየሱስ', 'ዓለምን እንዳላይ', 'ለጥሞና ጊዜ አጣሁኝ', 'መዝሙር ፳፪', 'የሚመጣ', 'ኢየሩሳሌም', 'ወጥመዱ ተሰበረ', 'ጭንቀቴን ላዋየው', 'ሮጦ ሮጦ', 'ዞር ብዬ ሳየው', 'ሰላም ወዴት አለሽ', 'ይቅርታን ለማድረግ', 'አሃዱ እላለሁኝ']
+      },
+      {
+        title: 'Zem Alelem',
+        amharic: 'ዝም አልልም',
+        lyricsUrl: 'https://wikimezmur.org/am/Dereje_Kebede',
+        songs: ['ወረት አያውቀው ጌታዬ', 'ዝንጀሮ አይደለሁም', 'የማይደፈርስ ሰላም አለኝ', 'ናፍቆታቸው ሰርጾብኛል', 'ዝም አልልም', 'ጋርዳቸው ከመታከት', 'ይሰባሰባሉ', 'ይቅርታን ለማድረግ', 'መከራ ሳይመጣ', 'ጭንቀቴን ላዋየው', 'ጸልይለት']
+      }
+    ]);
+
+    return { bio, albums };
+  }
+}).mount('#app');
